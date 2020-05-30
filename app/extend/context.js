249055c77ca49e35a938785c28771845827c90e0
xjf <1015599722@qq.com>
2020-05-30 18:27:35 +0800

    添加参数校验器

--- app/extend/context.js
+++ app/extend/context.js
@@ -1,25 +1,10 @@
 'use strict';
 
-const Joi = require('@hapi/joi');
 
-const JOI = Symbol('paras checker');
 
 module.exports = {
-  
-  
-  validate2(schema,valueObj = this.request.body){
-    const {error,value} = Joi.validate(valueObj,schema);
-    if (!error) return value;
-    error.msg = error.message;
-    this.throw(422,error);
-    
-  },
-  
-  get Joi(){
-    
-    if (!this[JOI]) this[JOI] = Joi;
-    return this[JOI];
-  }
-  
-  
+
+
+
+
 };
--- app/extend/helper.js
+++ app/extend/helper.js
@@ -1,3 +1,8 @@
+'use strict';
+
+const Joi = require('@hapi/joi');
+
+const JOI = Symbol('paras checker');
 
 module.exports = {
 
@@ -17,5 +22,20 @@ module.exports = {
 
     return obj;
   },
-
+  
+  
+  validate2(schema,valueObj = this.request.body){
+    const {error,value} = Joi.validate(valueObj,schema);
+    if (!error) return value;
+    error.msg = error.message;
+    this.throw(422,error);
+    
+  },
+  
+  get Joi(){
+    
+    if (!this[JOI]) this[JOI] = Joi;
+    return this[JOI];
+  }
+  
 };
