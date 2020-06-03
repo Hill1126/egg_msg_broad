86c81a2ab5c3361b9506ad940e6219c6d0114fe1
xjf <1015599722@qq.com>
2020-06-03 19:51:30 +0800

    新增参数校验单元测试

--- test/app/utils/utils.test.js
+++ test/app/utils/utils.test.js
@@ -5,25 +5,27 @@ const { app, assert } = require('egg-mock/bootstrap');
 describe('util test', () => {
   
   
+  const obj = {};
   
-  const obj = {
+  before(()=>{
+    Object.assign(obj,data);
+  });
+  
+  const data = {
     username: '1015599722',
     name: 'xujunfeng',
   }
   
-  it('should Checker works fine', function () {
-    try{
   
-     const ctx = app.mockContext();
-     
-     
-      const schema = {
-        username : ctx.Joi.string().required(),
-        password : ctx.Joi.string().min(5).required(),
-        name     : ctx.Joi.string().default('22'),
-      }
-      
-      ctx.helper.validate2(schema,obj);
+  it('should Checker works fine', function () {
+    const ctx = app.mockContext();
+    let schema = {
+      username : ctx.Joi.string().required(),
+      password : ctx.Joi.string().min(5).required(),
+      name     : ctx.Joi.string().default('22'),
+    };
+    try{
+      ctx.validate2(schema,obj);
     } catch (e) {
       console.log(e);
       return;
@@ -31,5 +33,26 @@ describe('util test', () => {
     assert.fail('校验工具失效')
     
   });
+  
+  it('should check pass', function () {
+    const ctx = app.mockContext();
+    obj.password = 'password';
+    let schema = {
+      username : ctx.Joi.string().required(),
+      password : ctx.Joi.string().min(5).required(),
+      name     : ctx.Joi.string().default('22'),
+    };
+    try{
+      const value = ctx.validate2(schema,obj);
+      assert(value);
+    } catch (e) {
+      assert.fail('校验发生错误:',e.msg);
+      return;
+    }
+   
+    
+  });
+  
+  
   
 });
