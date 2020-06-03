207936ad297b5fb2c499dfe3c56cfc9ed1a11a01
xjf <1015599722@qq.com>
2020-06-03 19:51:54 +0800

    使用joi实现参数校验方法

--- app/extend/context.js
+++ app/extend/context.js
@@ -1,10 +1,22 @@
 'use strict';
 
-
+const Joi = require('joi');
+const JOI = Symbol('paras checker');
 
 module.exports = {
-
-
-
+  
+  validate2(schema,valueObj = this.request.body){
+    const {error,value} = Joi.validate(valueObj,schema);
+    if (!error) return value;
+    error.msg = error.message;
+    this.throw(422,error);
+    
+  },
+  
+  get Joi(){
+    
+    if (!this[JOI]) this[JOI] = Joi;
+    return this[JOI];
+  }
 
 };
--- app/extend/helper.js
+++ app/extend/helper.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const Joi = require('@hapi/joi');
-
-const JOI = Symbol('paras checker');
 
 module.exports = {
 
@@ -23,19 +20,6 @@ module.exports = {
     return obj;
   },
   
-  
-  validate2(schema,valueObj = this.request.body){
-    const {error,value} = Joi.validate(valueObj,schema);
-    if (!error) return value;
-    error.msg = error.message;
-    this.throw(422,error);
-    
-  },
-  
-  get Joi(){
-    
-    if (!this[JOI]) this[JOI] = Joi;
-    return this[JOI];
-  }
+
   
 };
