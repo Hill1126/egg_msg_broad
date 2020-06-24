1d6309af950ed3976f463976918b8fa731c25f6b
xjf <1015599722@qq.com>
2020-06-24 16:57:00 +0800

    复用header代码 初步完成修改密码页面、留言管理页面

---
+++ app/public/js/utils.js
@@ -0,0 +1,31 @@
+
+
+document.getElementById('header').innerHTML = `
+ <div class="layui-layout layui-layout-admin">
+  <div class="layui-header">
+    <div class="layui-logo">egg 在线留言板</div>
+    <ul class="layui-nav layui-layout-left">
+      <li class="layui-nav-item">
+        <a href="index.html">首页</a>
+      </li>
+    </ul>
+
+    <ul class="layui-nav layui-layout-right">
+      <li class="layui-nav-item">
+        <a href="myMsg.html">我的留言</a>
+      </li>
+      <li class="layui-nav-item">
+        <a href="javascript:;">
+          <img src="http://t.cn/RCzsdCq" class="layui-nav-img">
+          贤心
+        </a>
+        <dl class="layui-nav-child">
+          <dd><a href="user.html">个人中心</a></dd>
+          <dd><a href="changePass.html">修改密码</a></dd>
+        </dl>
+      </li>
+      <li class="layui-nav-item"><a href="">退出</a></li>
+    </ul>
+  </div>
+</div>
+`;
